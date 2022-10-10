fa95b9b7a3494672ecb1a1b90e3551581d700fc2
willy da silva <willydasilva29@gmail.com>
2022-10-10 10:12:49 -0300

    adicionado crossOrigin

--- Projeto-final-API/src/main/java/com/projetofinalapi/ProjetoFinalApiApplication.java
+++ Projeto-final-API/src/main/java/com/projetofinalapi/ProjetoFinalApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin("*")
+
 public class ProjetoFinalApiApplication {
 
 	public static void main(String[] args) {
--- Projeto-final-API/src/main/java/com/projetofinalapi/controller/RecadosController.java
+++ Projeto-final-API/src/main/java/com/projetofinalapi/controller/RecadosController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/recados")
+@CrossOrigin("*")
 public class RecadosController {
 
     @Autowired
